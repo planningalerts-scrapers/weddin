70bb0081bab2b47cfcd93496a9ddfc0267084610
Matthew Landauer <matthew@oaf.org.au>
2015-03-19 12:12:41 +1100

    update gems and update path to match changes in atdis gem

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-ATDISPlanningAlertsFeed.save('http://myhorizon.solorient.com.au/Horizon/weddin/atdis/1.0/applications.json')
+ATDISPlanningAlertsFeed.save('http://myhorizon.solorient.com.au/Horizon/weddin/atdis/1.0')
