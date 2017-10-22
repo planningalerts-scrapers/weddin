a3b8687f0ffcb115916aec46ca39b1fbe7a68b51
Eric Tam <eric.tam@traceyanderic.com>
2017-10-22 20:37:33 +1100

    moved away from ATDIS and collect data via web site's XML

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,15 @@
-#!/usr/bin/env ruby
-Bundler.require
+require 'scraperwiki'
+require 'horizon_xml'
+ENV['MORPH_PERIOD'] = '2017'
+collector = Horizon_xml.new
+collector.setPeriod(ENV['MORPH_PERIOD']).setDomain('horizondap').setInfoUrl(collector.host_url).setCommentUrl('mailto:mail@weddin.nsw.gov.au')
 
-ATDISPlanningAlertsFeed.save('http://myhorizon.solorient.com.au/Horizon/weddin/atdis/1.0')
+collector.getRecords.each do |record|
+#   p record
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+    puts "Saving record " + record['council_reference'] + ", " + record['address']
+    ScraperWiki.save_sqlite(['council_reference'], record)
+  else
+    puts "Skipping already saved record " + record['council_reference']
+  end
+end
